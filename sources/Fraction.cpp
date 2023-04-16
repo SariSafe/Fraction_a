e245de1a1803d066dfc35014ea66e37696ba40f3
97253 <sygw6457@gmail.com>
2023-04-16 23:50:13 +0300

    start

---
+++ sources/Fraction.cpp
@@ -0,0 +1,18 @@
+/**
+ *declare about implemention and logic of the structure of program
+ *@author Sari Safe
+ *@since 16.04.23
+ */
+
+#include <iostream>
+#include <stdexcept>
+#include "Fraction.hpp"
+
+namespace ariel
+{
+
+    class Fraction
+    {
+    };
+
+};
---
+++ sources/Fraction.hpp
@@ -0,0 +1,19 @@
+/**
+ *declare about  the structure of program
+ *@author Sari Safe
+ *@since 16.04.23
+ */
+
+// to dont include the header alot, once at most
+#pragma once
+
+#include <iostream>
+
+namespace ariel
+{
+
+    class Fraction
+    {
+    };
+
+};
